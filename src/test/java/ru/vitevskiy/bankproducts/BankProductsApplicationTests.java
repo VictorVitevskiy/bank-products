5389c9b3f63c5765591e553650b352f83a337911
Victor Vitevskiy <vitevskiy@mail.ru>
2023-01-26 23:27:31 +0400

    Init commit

---
+++ src/test/java/ru/vitevskiy/bankproducts/BankProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vitevskiy.bankproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
